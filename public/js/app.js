ff35bd8ab36ea974abaa94bd5f2d9e7b56f507e9
sebcsaba <sebcsaba@gmail.com>
2017-01-10 21:06:03 +0100

    display dialog

--- public/js/app.js
+++ public/js/app.js
@@ -11,12 +11,14 @@ function Model() {
 			id: id,
 			email: email
 		};
-		$.getJSON(this.api, data, cb);
+//		$.getJSON(this.api+'/reserve', data, cb);
+		cb();
 	};
 
 }
 
 function App() {
+	app = this;
 
 	this.model = new Model();
 
@@ -28,5 +30,34 @@ function App() {
 		$('<ul/>', { html: items.join('') }).appendTo('#list');
 	}
 	this.model.list(initList);
+
+	this.dialog = $('#dialog-form').dialog({
+		autoOpen: false,
+		modal: true,
+		height: 200,
+		width: 400,
+		modal: true,
+		buttons: {
+//			"Lefoglalom!": app.onSubmit,
+			'Mégsem': function(){
+				app.dialog.dialog('close');
+			}
+		}
+	});
+
+	$('#list').on('click', 'li', function(){
+		var id = $(this).attr('id');
+		var text = $(this).html();
+		$('#dialog-form input[name=id]').val(id);
+		app.dialog.dialog('option','title',text);
+		app.dialog.dialog('open');
+	});
+
+	this.onSubmit = function() {
+		var id = $('#dialog-form input[name=id]').val();
+		var email = $('#dialog-form input[name=email]').val();
+		app.model.reserve(id, email, function(){});
+		app.dialog.dialog('close');
+	};
 
 }
