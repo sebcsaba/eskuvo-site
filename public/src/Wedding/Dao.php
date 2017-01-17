1f91e75b50860aaa20af1ac22418c8902202f5d4
sebcsaba <sebcsaba@gmail.com>
2017-01-17 20:28:56 +0100

    add reserve and cancel functions to dao

--- public/src/Wedding/Dao.php
+++ public/src/Wedding/Dao.php
@@ -22,6 +22,30 @@ class Dao
 		return $this->fetchAll("SELECT id, description FROM wedding_list WHERE email IS NULL ORDER BY description");
 	}
 
+	public function reserveWish($id, $email, $code)
+	{
+		$sql = 'UPDATE wedding_list SET email=:email, verification_code=:code WHERE id=:id AND email IS NULL';
+		$stmt = $this->db->prepare($sql);
+		$stmt->bindParam('email', $email, PDO::PARAM_STR);
+		$stmt->bindParam('code', $code, PDO::PARAM_STR);
+		$stmt->bindParam('id', $id, PDO::PARAM_INT);
+		if (!$stmt->execute()) {
+			throw new Exception(var_export($stmt->errorInfo(),true));
+		}
+	}
+
+	public function cancelWish($id, $email, $code)
+	{
+		$sql = 'UPDATE wedding_list SET email=null, verification_code=null WHERE id=:id AND email=:email AND verification_code=:code';
+		$stmt = $this->db->prepare($sql);
+		$stmt->bindParam('email', $email, PDO::PARAM_STR);
+		$stmt->bindParam('code', $code, PDO::PARAM_STR);
+		$stmt->bindParam('id', $id, PDO::PARAM_INT);
+		if (!$stmt->execute()) {
+			throw new Exception(var_export($stmt->errorInfo(),true));
+		}
+	}
+
 	private function fetchAll($query)
 	{
 		$stmt = $this->db->query($query);
