4bb0255949c9fad8841caf424ee24c1061da7bef
fqqdk <fqqdk1@gmail.com>
2017-01-17 22:26:10 +0100

    update

--- public/src/Util/Mail.php
+++ public/src/Util/Mail.php
@@ -14,6 +14,12 @@ class Mail
 	 */
 	private $content;
 
+	/**
+	 * @var array
+	 */
+	private $headers = [];
+
+
 	public static function create($subject, $content)
 	{
 		return new self($subject, $content);
@@ -25,8 +31,35 @@ class Mail
 		$this->content = $content;
 	}
 
+	public function html($charSet = 'UTF-8')
+	{
+		return $this->addHeader('Content-Type', "text/html; charset=$charSet");
+	}
+
+	public function from($email)
+	{
+		return $this->addHeader('From', $email);
+	}
+
+	public function replyTo($email)
+	{
+		return $this->addHeader('Reply-To', $email);
+	}
+
 	public function sendTo(array $recipients)
 	{
-		mail(implode(', ', $recipients), $this->subject, $this->content);
+		$this->addHeader('MIME-Version', '1.0');
+
+		mail(implode(', ', $recipients), $this->subject, $this->content, implode("\r\n", array_map(
+			function ($key, $value) { return "$key: $value"; },
+			array_keys($this->headers),
+			array_values($this->headers)
+		)));
+	}
+
+	private function addHeader($key, $value)
+	{
+		$this->headers[$key] = $value;
+		return $this;
 	}
 }
